df76e77e8f65ea7cc7ee0c0df41e38817e82bf5b
Dinu Sergiu-Andrei <dinusergiuandrei@yahoo.com>
2018-03-02 20:52:02 +0200

    Added JUnit.

--- tests/Parser/PgnFileParserTest.java
+++ tests/Parser/PgnFileParserTest.java
@@ -1,9 +1,12 @@
 package Parser;
 
+import org.junit.Test;
+
 import java.util.List;
 
 public class PgnFileParserTest {
-    public static void main(String args[]) {
+    @Test
+    public void parseGamesTest() {
         PgnFileParser pgnFileParser = new PgnFileParser("database/players/Christiansen.pgn");
         List<GameDetails> games = pgnFileParser.parseGames();
         System.out.println(games.get(1).toString());
